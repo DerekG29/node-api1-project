6876b090d9bcb175cf6e83d76e7161341ab3cbf5
DerekG29 <derekgrubbs29@gmail.com>
2024-06-29 13:40:56 -0400

    Can successfully GET a user by a specific ID

--- api/server.js
+++ api/server.js
@@ -18,7 +18,20 @@ server.get('/api/users', async (req, res) => {
       res.status(404).json({ message: 'No users found...' });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Error GETing users...' });
+    res.status(500).json({ message: 'The users information could not be retrieved' });
+  }
+})
+
+server.get('/api/users/:id', async (req, res) => {
+  try {
+    const user = await db.findById(req.params.id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: 'The user with the specified ID does not exist' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'The user information could not be retrieved' });
   }
 })
 
