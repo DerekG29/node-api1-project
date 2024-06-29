5d90b7d2d8f17a6abd5dfef39bb909a3d345cf64
DerekG29 <derekgrubbs29@gmail.com>
2024-06-29 13:25:15 -0400

    can GET all users successfully

--- api/server.js
+++ api/server.js
@@ -1,11 +1,25 @@
 // BUILD YOUR SERVER HERE
+const db = require('./users/model');
 const express = require('express');
 
 const server = express();
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  res.status(200).json({ message: 'testing' });
+server.get('/', async (req, res) => {
+  res.status(200).json({ message: 'hello world' });
+})
+
+server.get('/api/users', async (req, res) => {
+  try {
+    const users = await db.find();
+    if (users.length) {
+      res.status(200).json(users);
+    } else {
+      res.status(404).json({ message: 'No users found...' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error GETing users...' });
+  }
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
